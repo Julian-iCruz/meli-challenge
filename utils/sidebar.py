4388a338ba12cf2ad79c5bcf24310c666874aa71
Julian-iCruz <julian.iguavita@outlook.com>
2023-04-30 02:00:00 -0500

    Filter to select maxima and minima of numerical variables

--- utils/sidebar.py
+++ utils/sidebar.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 import pandas as pd
 import streamlit as st
 from datetime import time
@@ -20,30 +19,21 @@ def generateSidebar():
     columns_type = getColumnsType(df)
     df[columns_type["categorical_columns"]] = df[columns_type["categorical_columns"]].astype(str)
     
-    st.sidebar.header("Filtros generales")
-    with st.sidebar.expander("Delete columns"):
-        df, columns_drop = deleterColumns(df, columns_type)
-    
-    with st.sidebar.expander("Time range"):
-        df = selectTimeRange(df, columns_type["datetime_columns"], columns_drop["datetime_drop"])
-    
-    with st.sidebar.expander("Categorical columns"):
-        df = filterCategoricalColumns(df, columns_type["categorical_columns"], columns_drop["categorical_drop"])
-        
-    st.write(df.shape)
-    st.write(df)
-
+    df, columns = filterDataset(df, columns_type)
+    return df, columns
 
 def uploadFiles(folder_path):
     uploaded_file = st.file_uploader(label="Load dataset to be explored", type=["csv"], help="")
-    if uploaded_file:
-        df = pd.read_csv(uploaded_file)
-        file_path = os.path.join(folder_path, uploaded_file.name)
-        if not os.path.exists(file_path):
-            df.to_csv(file_path, index=False)
-            st.success("Successfully saved")
-        else:
-            st.warning("Already existing file", icon="🚨")
+    if not uploaded_file:
+        return
+    
+    df = pd.read_csv(uploaded_file)
+    file_path = os.path.join(folder_path, uploaded_file.name)
+    if not os.path.exists(file_path):
+        df.to_csv(file_path, index=False)
+        st.success("Successfully saved")
+    else:
+        st.warning("Already existing file", icon="🚨")
     return
 
 def selectFile(folder_path):
@@ -73,10 +63,31 @@ def getDateTypeColumns(df):
             pass
     return list_date_type_columns
 
-def selectTimeRange(df, columns_datetime, columns_drop):
+def filterDataset(df, columns_type):
+    st.sidebar.header("Filtros generales")
+    with st.sidebar.expander("Delete columns"):
+        df, columns_drop = deleterColumns(df, columns_type)
+    
+    columns = {
+        "datetime": set(columns_type["datetime_columns"]) - set(columns_drop["datetime_drop"]),
+        "categorical": set(columns_type["categorical_columns"]) - set (columns_drop["categorical_drop"]),
+        "numerical": set(columns_type["numerical_columns"]) - set(columns_drop["numerical_drop"]),
+    }
+
+    with st.sidebar.expander("Time range"):
+        df = selectTimeRange(df, columns["datetime"])
+    
+    with st.sidebar.expander("Categorical columns"):
+        df = filterCategoricalColumns(df, columns["categorical"])
+    
+    with st.sidebar.expander("Numerical columns"):
+        df = filterNumericalColumns(df, columns["numerical"])
+    return df, columns
+
+def selectTimeRange(df, columns_datetime):
     datetime_columns = []
     if len(columns_datetime) != 0:
-        datetime_columns = set(columns_datetime) - set(columns_drop)
+        datetime_columns = columns_datetime
     time_selector = st.selectbox("Select the datetime variables", datetime_columns, index=0)
 
     if time_selector == None:
@@ -108,11 +119,24 @@ def deleterColumns(df, columns_type):
         "numerical_drop": numerical_drop,
     }
 
-def filterCategoricalColumns(df, columns_categorical, columns_drop):
-    for column in set(columns_categorical) - set(columns_drop):
+def filterCategoricalColumns(df, columns_categorical):
+    for column in columns_categorical:
         categorical_selector = st.multiselect(column, df[column].value_counts().keys(), key = column)
         if len(categorical_selector) != 0:
             df = df[df[column].isin(categorical_selector)]
+    return df
+
+def filterNumericalColumns(df, columns_numerical):
+    for column in columns_numerical:
+        max, min = [df[column].max(), df[column].min()]
+        st.write(column)
+        col1, col2 = st.columns(2)
+        with col1:
+            max_value = st.number_input('Maximum value', max_value=max, value=max, key=column+"_max")
+        with col2:
+            min_value = st.number_input('Minimum value', min_value=min, value=min, key=column+"_min")
+        st.divider()
+        df = df[(df[column]>=min_value) & (df[column]<=max_value)]
     return df
 
 def deleteFile(file, folder_path):
