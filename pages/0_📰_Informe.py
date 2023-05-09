4e3ac0650855dc73d78bcf0a7ad66365d021e1f4
Julian-iCruz <julian.iguavita@outlook.com>
2023-05-09 00:18:33 -0500

    Factoring first deliverable report

--- pages/0_📰_Informe.py
+++ pages/0_📰_Informe.py
@@ -1,9 +1,11 @@
 import os 
 import json
 import pandas as pd
+from PIL import Image
 import streamlit as st
 from utils.informe import *
 from utils.sidebar import generateSidebar
+from utils.utils import downloadData, joinData
 
 st.set_page_config(page_title="Informe", page_icon="📰")
 
@@ -12,12 +14,36 @@ with open('texts.json', 'r') as archivo:
 
 df, columns = generateSidebar()
 
+intro = texts['informe']['intro']
 st.title('Análisis exploratorio de datos | EDA 📊')
-st.markdown(texts['informe']['EDA'])
+st.markdown(intro['join'])
+st.code(intro['code_join'], language='python')
+st.markdown(intro['after_join'])
+downloadData(joinData())
+st.divider()
 
+st.subheader('Análisis descriptivo 📊')
+st.markdown(texts['informe']['descriptivo']['observations'])
+try:
+    len(df)
+    st.dataframe(df)
+except:
+    st.warning("Proceed to load the dataset you downloaded earlier.", icon="🚨")
+st.divider()
+
+st.subheader('Análisis categorico 📊')
+categorical = texts['informe']['categorical']
+st.markdown(categorical['univariate'])
+st.markdown(categorical['bivariate'])
+st.markdown(categorical['multivariate'])
+st.divider()
+
+#image = Image.open('img/newplot.png')
+#st.image(image, caption='Sunrise by the mountains')
 st.title('Procesamiento de datos | 🛠')
-st.markdown(texts['informe']['Procesamiento'])
-st.code(texts['informe']['Code'], language='python')
+processing = texts['informe']['processing']
+st.markdown(processing['target_1'])
+st.code(processing['code_1'], language='python')
 st.write('Obtenemos el siguiente dataset procesado.')
 
 if columns != None:
@@ -29,34 +55,8 @@ if columns != None:
 else:
     st.warning("Choose the dataset to be processed with the above code", icon="🚨")
 
-st.markdown(texts['informe']['Procesamiento_1'])
+st.markdown(processing['posprocessing_1'])
 st.divider()
 
-st.title('Preguntas Challenge | ❔')
-st.markdown("""
-* Indicar qué información necesitarías para mejorar/ampliar tu entregable.
-
-Bueno, creo que con un poco más de tiempo se podrían lograr grandes cambios en la aplicación que planteo como solución, ya que sería una plataforma que minimizaría tiempo y esfuerzo a la hora de crear análisis exploratorio de datos con respecto a las formas tradicionales de hacerlo, incluso se podría llegar un nivel más alto de automatización donde se generen informes que contextualicen al usuario de la información más importante del conjunto de datos.
-
-De igual manera, se podría crear una sección de modelos que pueda realizar pruebas y sacar métricas de estos de manera interactiva.
-
-* ¿Cómo crees que podría mejorarse el dataset desde la creación del mismo si lo tuvieses que procesar?
-
-Se podrían traer algunas variables clasificatorias para los usuarios que ayudarían a segmentar la población de buena manera, por ejemplo:
-
-1. Edad
-2. Genero
-3. Profesion
-4. Informacion demografica
-
-Tambien se puede establecer un protocolo de calidad de datos que puede ayudar a garantizar que los datos se recopilen y procesen de manera consistente y precisa, lo que aumenta la confiabilidad y la validez del dataset.
-
-
-* ¿Cuáles serían las ventajas de poseer labels en este dataset?
-
-La ventaja principal es que podrían implementar técnicas de aprendizaje supervisado, esto también ayuda a comprender mejor la estructura y patrones de los datos. De igual manera, pueden mejorar la precisión de los resultados obtenidos en los modelos, al igual que hay un ahorro de tiempo y recursos para evitar etiquetar datos manualmente o con técnicas de aprendizaje no supervisado.
-
-* ¿De poseer aún más datos relacionados con el consumo de dicho site, crees que podríamos identificar ataques sobre el mismo?¿De qué tipo?.
-
-Si, en lo absoluto, a pesar de que ya se tiene una gran cantidad de datos, puede que en un caso de uso donde se busque entender el comportamiento de cada usuario estos datos se queden cortos, en un sistema de modelos escalonados donde primero se determina si un evento es anómalo entre todo el dataset o de todos los eventos generados y luego un modelo especifico por usuario el tener más datos por usuario mejora el entendimiento de la línea base de comportamiento del mismo.
-""")
+st.title('Preguntas Challenge |❔')
+st.markdown(texts['informe']['questions'])
--- utils/utils.py
+++ utils/utils.py
@@ -11,18 +11,14 @@ def joinData():
     df_firts_round = deleteNullRowsAndColumns(df_firts_round_raw)
     df_second_round_raw = deleteNullRowsAndColumns(df_firts_round_raw)
     df_behavior = pd.concat([df_firts_round, df_second_round_raw], axis = 0)
-    df_behavior["Date_2"] = df_behavior["Date"]
-    df_behavior["Number_int"] = 100
-    df_behavior["Number_float"] = 5.0
-    df_behavior["bool"] = True
     return df_behavior
 
 @st.cache_data
-def convert_df(df):
+def convertDf(df):
     return df.to_csv(index=False).encode("utf-8")
 
 def downloadData(df):
-    csv = convert_df(df)
+    csv = convertDf(df)
     title = st.text_input("Nombre de descarga de su archivo", "Data_Experiment")
     st.download_button(label="Download data as CSV", data = csv, file_name = str(title)+".csv", mime="text/csv",)
     return
@@ -37,74 +33,3 @@ def getInfo(df):
     for column in df.columns:
         describe[column] = [str(df[column].dtype), df[column].isnull().sum()]
     return describe
-
-def getVariableType(df):
-    time_variable = []; numerical_variable = []; categorical_variable = []
-    df_date = pd.DataFrame({"Date": ["2022-05-01"],"Categorica": ["Categoria"], "Numerica": 10})
-    df_date["Date"] = pd.to_datetime(df_date["Date"])
-    df["Date"] = pd.to_datetime(df["Date"])
-    
-    for column in df.columns:
-        if df_date["Date"].dtype == df[column].dtype:
-            time_variable.append(column)
-        elif df_date["Categorica"].dtype == df[column].dtype:
-            categorical_variable.append(column)
-        elif df_date["Numerica"].dtype == df[column].dtype:
-            numerical_variable.append(column)
-            
-    dict_type_variables = {
-        "time_variable" : time_variable,
-        "categorical_variable" : categorical_variable,
-        "numerical_variable" : numerical_variable,
-    }
-    return dict_type_variables
-
-def generateSidebar(df, variable_type):
-    st.sidebar.header("Filtros generales")
-    st.sidebar.divider()
-    star_date_input = ""; end_date_input = ""; time_range = ""
-
-    with st.sidebar.expander("Rango de Tiempo"):
-        st.divider()
-        if len(variable_type["time_variable"]) == 1:
-            time_selector = variable_type["time_variable"][0]
-        
-        if len(variable_type["time_variable"]) > 1:
-            time_selector = st.sidebar.selectbox("Variables de Tiempo", df[variable_type["time_variable"]].columns)
-        
-        if len(variable_type["time_variable"]) != 0:
-            start_date = df[time_selector].min().date()
-            end_date = df[time_selector].max().date()
-
-            col1, col2 = st.columns(2)
-
-            with col1:
-                start_date_input = st.date_input("Fecha de inicio", start_date)
-
-            with col2:
-                end_date_input = st.date_input("Fecha de fin", end_date)
-
-            time_range = st.slider("Rango de hora", value=(time(00, 00), time(23, 59)))
-
-    with st.sidebar.expander("Eliminar Columnas"):
-        st.divider()
-        categorical_drop = st.multiselect("Variables Categóricas", variable_type["categorical_variable"])
-        numerical_drop = st.multiselect("Variables Numerica", variable_type["numerical_variable"])
-
-    with st.sidebar.expander("Variables Categóricas"):
-        st.divider()
-        for column in variable_type["categorical_variable"]:
-            categorical_selector = st.multiselect(column, df[column].value_counts().keys(), key = column)
-            if len(categorical_selector) != 0:
-                df = df[df[column].isin(categorical_selector)]
-                
-    
-    with st.sidebar.expander("Variables Numericas"):
-        st.divider()
-        numerical_selector = st.multiselect("Variables Categóricas", variable_type["numerical_variable"])
-
-    start_date = pd.to_datetime(start_date_input)
-    end_date = pd.to_datetime(end_date_input)
-    df_behavior = df[(df["Date"] >= start_date) & (df["Date"] <= end_date)]
-    df_behavior = df_behavior.drop(categorical_drop, axis = 1)
-    return df_behavior
